efa58a105a5b5fb39fdc222d58adb55e8f0f1d1e
Jason Golden <jasonwilliamgolden@gmail.com>
2025-04-25 09:48:27 -0400

    ok

--- react-dashboard/src/components/DepressionDashboard.jsx
+++ react-dashboard/src/components/DepressionDashboard.jsx
@@ -1,1 +1,12 @@
-ok
+import React from 'react';
+import DepressionDashboard from './components/DepressionDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <DepressionDashboard />
+    </div>
+  );
+}
+
+export default App;
